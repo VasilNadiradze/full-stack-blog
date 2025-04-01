cf1c939ef6897949c553f3e51ea5bf21a8478699
VasilNadiradze <vasil.nadiradze@gmail.com>
2025-04-01 22:02:26 +0400

    Removed unnecessary files

--- src/routes/index.js
+++
@@ -1,6 +0,0 @@
-export { default as HomePage } from "./HomePage";
-export { default as PostListPage } from "./PostListPage";
-export { default as SinglePostPage } from "./SinglePostPage";
-export { default as WritePage } from "./WritePage";
-export { default as LoginPage } from "./LoginPage";
-export { default as RegisterPage } from "./RegisterPage";
